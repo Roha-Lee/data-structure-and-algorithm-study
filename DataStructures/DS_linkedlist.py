d10e8bedc76d38c5fbc9cd6a91a6f6d574856a8f
Lee JongHo <rohagru@gmail.com>
2021-09-10 19:40:03 +0900

    Add pop(index) function(allow arbitrary position to pop)

--- DataStructures/DS_linkedlist.py
+++ DataStructures/DS_linkedlist.py
@@ -27,17 +27,36 @@ class LinkedList:
         if location == 0:
             self.head = Node(data)
             self.head.next = curr
-            return 
-
+            return
         index = 0
         while curr.next and not index == location - 1:
             curr = curr.next
-            index += 1
-            
+            index += 1            
         new_node = Node(data)
         new_node.next = curr.next
         curr.next = new_node
-        
+
+    def pop(self, location=-1):
+        curr = self.head
+        prev = self.head
+        if not curr:
+            print("Empty LinkedList!")
+            return 
+    
+        if location == 0 or not curr.next:
+            self.head = curr.next
+            del curr
+            return 
+
+        index = 0
+        while curr.next and not index == location:
+            prev = curr
+            curr = curr.next 
+            index += 1
+        prev.next = curr.next
+        del curr
+
+                
 if __name__ == '__main__':
     # Link two different nodes.
     node1 = Node(1)
@@ -53,9 +72,28 @@ if __name__ == '__main__':
     print(linked_list)
     linked_list.push(4)
     print(linked_list)
+
+    # push test
     linked_list.push(5, 0)
     print(linked_list)
     linked_list.push(6, 1)
     print(linked_list)
     linked_list.push(7, 3)
     print(linked_list)
+
+    # pop test
+    linked_list.pop()
+    print(linked_list)
+    linked_list.pop(0)
+    print(linked_list)
+    linked_list.pop(2)
+    print(linked_list)
+    linked_list.pop(9)
+    print(linked_list)
+
+    # test2
+    linked_list2 = LinkedList(Node(1))
+    print(linked_list2)
+    linked_list2.pop()
+    print(linked_list2)
+    linked_list2.pop()
