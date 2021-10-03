ae481a9afe146d5593b2ac08dd936292dc4a821b
Roha-Lee <rohagru@gmail.com>
2021-10-03 19:43:21 +0900

    add get_path function

--- Algorithms/AL_dijkstra_algorithm.py
+++ Algorithms/AL_dijkstra_algorithm.py
@@ -2,30 +2,39 @@
 import heapq
 
 def dijkstra(graph, root):
-    # path_dict = dict()
-    # for key in graph.keys():
-    #     if key == root:
-    #         path_dict[key] = 0
-    #     else:
-    #         path_dict[key] = 'INF'
-    
-    path_dict = {node: float('inf') for node in graph}
-    path_dict[root] = 0
+    path_dict = {node: [float('inf'), node] for node in graph}
+    path_dict[root][0] = 0
     
     pri_q = []
     heapq.heappush(pri_q, (0, root))
     
     while pri_q:
         dist, node = heapq.heappop(pri_q)
-        if dist > path_dict[node]:
+        if dist > path_dict[node][0]:
             continue
         for neighbor, weight in graph[node].items():
-            if path_dict[neighbor] == 'INF' or path_dict[neighbor] > dist + weight:
+            if path_dict[neighbor][0] > dist + weight:
                 # 우선순위큐에 넣기 
                 heapq.heappush(pri_q, (dist + weight, neighbor))
                 # 거리 업데이트 
-                path_dict[neighbor] = dist + weight
+                path_dict[neighbor][0] = dist + weight
+                path_dict[neighbor][1] = node
+    
     return path_dict
+
+
+def get_path(path_dict, root, dest):
+    if root == dest:
+        return ""
+    node = dest
+    path = []
+    path.append(' 🚩 %s (dest)'%(dest))
+    while not root == path_dict[node][1]:
+        path.append(' 🚩 %s'%(path_dict[node][1]))
+        node = path_dict[node][1]
+    path.append('(start) %s'%(root))
+    path.reverse()
+    return ''.join(path)
 
 if __name__ == '__main__':
     graph = {
@@ -36,4 +45,7 @@ if __name__ == '__main__':
         'E': {'F': 1},
         'F': {'A': 5}
     }
-    print(dijkstra(graph, 'A'))
+    start = 'A'
+    result = dijkstra(graph, start)
+    for dest in graph.keys():
+        print(get_path(result, start, dest))
